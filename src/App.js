3a7835bb46d506fd40ad32c7e080e8806374348a
Marin Petrunić <marin.petrunic@gmail.com>
2019-02-12 13:50:30 +0100

    use login component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import { MythxLogin } from './Login/MythXLogin'
+import { Provider } from 'react-redux'
+import { store } from './_helpers/store'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Provider store={store}>
+        <div className="App">
+          <h1>MythX Security Verification</h1>
+          <MythxLogin/>
+        </div>
+      </Provider>
+    )
   }
 }
 
-export default App;
+export default App
