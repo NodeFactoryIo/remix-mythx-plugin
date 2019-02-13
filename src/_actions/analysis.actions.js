6bcd7a13a42749880bafda906a2e274f0f449f6e
Marin Petrunić <marin.petrunic@gmail.com>
2019-02-13 21:30:01 +0100

    add thunk for submitting contract to analization

---
+++ src/_actions/analysis.actions.js
@@ -0,0 +1,34 @@
+import { analysisConstants } from '../_constants'
+import { mythxService } from '../_services'
+
+export const analysisActions = {
+  analyzeContracts
+}
+
+function analyzeContracts (contracts, source) {
+
+  return dispatch => {
+    dispatch(request())
+
+    mythxService.submitContractsForAnalysation(contracts, source)
+      .then(requests => {
+        console.log({requests})
+        return dispatch({
+          type: analysisConstants.ANALYSIS_SUBMIT_SUCCESS,
+          payload: requests
+        });
+      })
+      .catch(error => {
+        return dispatch(failure(error.toString()))
+      })
+
+    function request () {return {type: analysisConstants.ANALYSIS_SUBMIT_REQUEST}}
+
+    function failure (error) {
+      return {
+        type: analysisConstants.ANALYSIS_SUBMIT_FAILURE,
+        error
+      }
+    }
+  }
+}
