94ace6d6dd282fdae1ce9e2bdfd7ec5ed4440d77
Marin Petrunić <marin.petrunic@gmail.com>
2019-02-13 23:10:55 +0100

    Updates

---
+++ precache-manifest.85e1979a238f11e89acc5d747d8d9379.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd5c123388fdbbca8e83",
+    "url": "/remix-mythx-plugin/static/js/runtime~main.fd5c1233.js"
+  },
+  {
+    "revision": "178cc230755cc8845f3a",
+    "url": "/remix-mythx-plugin/static/js/main.178cc230.chunk.js"
+  },
+  {
+    "revision": "05855ed64292f68b28bb",
+    "url": "/remix-mythx-plugin/static/js/2.05855ed6.chunk.js"
+  },
+  {
+    "revision": "178cc230755cc8845f3a",
+    "url": "/remix-mythx-plugin/static/css/main.37ee21d5.chunk.css"
+  },
+  {
+    "revision": "22796c79c18ba95d4bddd8cf1acc478a",
+    "url": "/remix-mythx-plugin/index.html"
+  }
+];
