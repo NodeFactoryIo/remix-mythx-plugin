d558e15fa72e2e43d18560ada22217dae6c8990d
Marin Petrunić <marin.petrunic@gmail.com>
2019-02-13 21:28:20 +0100

    enable thunk and saga

--- src/_helpers/store.js
+++ src/_helpers/store.js
@@ -1,14 +1,22 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import rootReducer from '../_reducers';
+import { applyMiddleware, createStore } from 'redux'
+import { createLogger } from 'redux-logger'
+import rootReducer from '../_reducers'
+import rootSaga from '../_sagas'
+import thunkMiddleware from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
 
-const loggerMiddleware = createLogger();
+const loggerMiddleware = createLogger()
+
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware()
 
 export const store = createStore(
   rootReducer,
   applyMiddleware(
+    loggerMiddleware,
     thunkMiddleware,
-    loggerMiddleware
+    sagaMiddleware,
   )
-);
+)
+
+sagaMiddleware.run(rootSaga)
