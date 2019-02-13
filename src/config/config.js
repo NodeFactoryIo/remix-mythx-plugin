2bed9cc19c48b05026dddff585d02a4904f1fc73
Marin Petrunić <marin.petrunic@gmail.com>
2019-02-13 21:29:15 +0100

    add remix origins to config

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,13 @@
 export default {
   MythX: {
     apiUrl: 'https://api.mythx.io/v1'
+  },
+  Remix: {
+    trustedOrigins: [
+      "http://remix-alpha.ethereum.org",
+      "http://remix.ethereum.org",
+      "https://remix-alpha.ethereum.org",
+      "https://remix.ethereum.org"
+    ]
   }
 }
